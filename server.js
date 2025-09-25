24273b01ae048d4e40e01e9824a1d64fd2b91868
klebermkardel <molinakardel@outlook.com.br>
2025-09-25 10:56:23 -0300

    feat: Cria rota principal

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 // Porta do Servidor
 const PORT = 3000;
 
+// ROTA PRINCIPAL
+
+app.get('/', (req, res) => {
+  res.send('<h1>API no ar!</h1>')  
+})
+
 // Inicia o servidor e o faz escutar na porta definida
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}. . Acesse em http://localhost:${PORT}`)
